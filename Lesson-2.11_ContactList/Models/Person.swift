aa0de9d82c7f204aa7f944162bfc61195bd54865
Iurii Kurinnoi <yur4kur@icloud.com>
2023-07-27 17:42:25 +0300

    Groups structure has been created, DataSource has been added, Person model has been drafted.

---
+++ Lesson-2.11_ContactList/Models/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Lesson-2.11_ContactList
+//
+//  Created by Юрий Куринной on 27.07.2023.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let phoneNumber: String
+    let email: String
+    
+    static func getPersons() -> [Person] {
+        var contactList: [Person] = []
+        
+        return contactList
+    }
+}
---
+++ Lesson-2.11_ContactList/Services/DataStore.swift
@@ -0,0 +1,56 @@
+//
+//  DataStore.swift
+//  Lesson-2.11_ContactList
+//
+//  Created by Юрий Куринной on 27.07.2023.
+//
+
+final class DataStore {
+    let names = [
+        "Leanne",
+        "Ervin",
+        "Clementine",
+        "Patricia",
+        "Chelsey",
+        "Dennis",
+        "Kurtis",
+        "Nicholas",
+        "Glenna",
+        "Clementina"]
+    
+    let surnames = [
+        "DuBuque",
+        "Reichert",
+        "Runolfsdottir",
+        "Weissnat",
+        "Schulist",
+        "Dietrich",
+        "Lebsack",
+        "Bauch",
+        "Howell",
+        "Graham"]
+    
+    let phoneNumbers = [
+        "1-770-736-8031",
+        "010-692-6593",
+        "1-463-123-4447",
+        "1-493-170-9623",
+        "44(254)954-1289",
+        "1-477-935-8478",
+        "1-210-067-6132",
+        "1-586-493-6943",
+        "44(775)976-6794",
+        "1-024-648-3804"]
+    
+    let emails = [
+        "Rey.Padberg@karina.biz",
+        "Chaim_McDermott@dana.io",
+        "Sherwood@rosamond.me",
+        "Telly.Hoeger@billy.biz",
+        "Karley_Dach@jasper.info",
+        "Lucio_Hettinger@annie.ca",
+        "Julianne.OConner@kory.org",
+        "Nathan@yesenia.net",
+        "Shanna@melissa.tv",
+        "Sincere@april.biz"]
+}
