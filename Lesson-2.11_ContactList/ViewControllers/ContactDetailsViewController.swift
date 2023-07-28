c6ddb5ee9e4de1ed8e3505f3cc3bc87d3e966749
Iurii Kurinnoi <yur4kur@icloud.com>
2023-07-28 16:25:28 +0300

    Main task completed.

---
+++ Lesson-2.11_ContactList/ViewControllers/ContactDetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ContactDetailsViewController.swift
+//  Lesson-2.11_ContactList
+//
+//  Created by Юрий Куринной on 28.07.2023.
+//
+
+import UIKit
+
+final class ContactDetailsViewController: UITableViewController {
+    
+    // MARK: - Public properties
+    
+    var person: Person!
+    
+    // MARK: - Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = String("\(person.name) \(person.surname)")
+    }
+
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+        case 0:
+            let phoneCell = tableView.dequeueReusableCell(withIdentifier: "phone", for: indexPath)
+            
+            var phoneContent = phoneCell.defaultContentConfiguration()
+            phoneContent.text = String("Phone: \(person.phoneNumber)")
+            phoneCell.contentConfiguration = phoneContent
+            
+            return phoneCell
+        
+        default:
+            let emailCell = tableView.dequeueReusableCell(withIdentifier: "email", for: indexPath)
+            
+            var emailContent = emailCell.defaultContentConfiguration()
+            emailContent.text = String("E-mail: \(person.email)")
+            emailCell.contentConfiguration = emailContent
+            
+            return emailCell
+        }
+    }
+}
--- Lesson-2.11_ContactList/ViewControllers/ContactListViewController.swift
+++ Lesson-2.11_ContactList/ViewControllers/ContactListViewController.swift
@@ -7,21 +7,14 @@
 
 import UIKit
 
-class ContactListViewController: UITableViewController {
+final class ContactListViewController: UITableViewController {
 
-    let contactList = Person.getPersons()
+    // MARK: - Private properties
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+    private let contactList = Person.getPersons()
+    
     // MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         contactList.count
     }
@@ -32,55 +25,18 @@ class ContactListViewController: UITableViewController {
         
         var content = cell.defaultContentConfiguration()
         content.text = String("\(contact.name) \(contact.surname)")
-        
         cell.contentConfiguration = content
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let contactDetailsVC = segue.destination as? ContactDetailsViewController
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        contactDetailsVC?.person = contactList[indexPath.row]
     }
-    */
+    
 
 }
